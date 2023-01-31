3fdbdacb97c9bdd9850dcd5c817297f566b45943
Talita <tt.munchen@gmail.com>
2023-01-31 15:44:23 -0300

    Create initial pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import Container from "./components/layout/Container"
+import Company from "./components/pages/Company"
+import Contact from "./components/pages/Contact"
+import Home from "./components/pages/Home"
+import NewProject from "./components/pages/NewProject"
+
 function App() {
   return (
-    <div className="App">
-      <h1>Dale</h1>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">About company</Link>
+        <Link to="/newproject">New Project</Link>
+      </ul>
+
+      <Container customClass='min-height'>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </Router>
   )
 }
 
---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,7 @@
+function NewProject() {
+  return (
+    <h1>New project</h1>
+  )
+}
+
+export default NewProject
