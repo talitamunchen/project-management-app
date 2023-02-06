abfaf811f08a59d8b36089255ec90e50e52bdd0c
Talita <tt.munchen@gmail.com>
2023-02-06 11:22:10 -0300

    Create POST method

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,37 @@
 import ProjectForm from '../project/ProjectForm'
+import { useNavigate } from 'react-router-dom'
 import './NewProject.css'
 
 function NewProject() {
+  const navigate = useNavigate()
+
+  function createPost(project) {
+    project.cost = 0
+    project.services = []
+
+    fetch("http://localhost:5000/projects", {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data)
+        //redirect
+        navigate('/projects', {
+          message: 'Project created!'
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className='newproject-container'>
       <h1>Create a new project</h1>
       <p>Create your project and add the services</p>
-      <ProjectForm btnText="Create project"/>
+      <ProjectForm btnText="Create project" handleSubmit={createPost}/>
     </div>
   )
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,7 @@
 function Projects() {
   return (
     <div>
-      <p>Projects</p>
+      <h1>Projects</h1>
     </div>
   )
 }
